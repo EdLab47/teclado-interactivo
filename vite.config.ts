03bd6741e9ddc4eaf59568650edfc529f3002b74
EdLab47 <etelizr@gmail.com>
2025-11-18 19:54:13 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 👇 muy importante para GitHub Pages
-  base: '/teclado-interactivo/',
+  base: '/teclado-interactivo/',  // 👈 IMPORTANTE
   plugins: [react()],
   build: {
-    outDir: 'dist', // carpeta donde Vite deja los archivos estáticos
+    outDir: 'dist', // carpeta generada
   },
 })
+
